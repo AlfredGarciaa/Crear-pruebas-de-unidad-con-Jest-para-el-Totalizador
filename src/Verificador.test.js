8e623f6da6c88050bad2d6bcc6843992320dd1b7
Alfred Garcia <alfred.garcia@ucb.edu.bo>
2022-03-22 02:34:26 -0400

    calcular descuentos de todos

--- src/Verificador.test.js
+++ src/Verificador.test.js
@@ -1,7 +1,8 @@
-import CalcularImpuesto from "./Calcular_Funciones.js";
+import CalcularImpuesto from "./Calcular_Impuestos.js";
+import CalcularDescuento from "./Calcular_Descuentos.js";
 
 describe("==============    /// IMPUESTOS ///   ==============", () => {
-  // if resultado x == x then mostrar mensaje de exito (verde)
+  // if resultado X == X then mostrar mensaje de exito (verde)
   // else mostrar mensaje de falla (rojo)
   it("deberia calcular el impuesto del estado UT", () => {
     const resultado = CalcularImpuesto(10,"UT");
@@ -22,5 +23,30 @@ describe("==============    /// IMPUESTOS ///   ==============", () => {
   it("deberia calcular el impuesto del estado CA", () => {
     const resultado = CalcularImpuesto(50,"CA");
     expect(resultado).toEqual(4.125); 
+  });
+});
+
+describe("==============    /// DESCUENTOS ///   ==============", () => {
+  // if resultado X == X then mostrar mensaje de exito (verde)
+  // else mostrar mensaje de falla (rojo)
+  it("deberia calcular el descuento mayor a 1000", () => {
+    const resultado = CalcularDescuento(1500);
+    expect(resultado).toEqual(45); 
+  });
+  it("deberia calcular el descuento mayor a 3000", () => {
+    const resultado = CalcularDescuento(5000);
+    expect(resultado).toEqual(250); 
+  });
+  it("deberia calcular el descuento mayor a 7000", () => {
+    const resultado = CalcularDescuento(8500);
+    expect(resultado).toEqual(595); 
+  });
+  it("deberia calcular el descuento mayor a 10000", () => {
+    const resultado = CalcularDescuento(20000);
+    expect(resultado).toEqual(2000); 
+  });
+  it("deberia calcular el descuento mayor a 30000", () => {
+    const resultado = CalcularDescuento(100000);
+    expect(resultado).toEqual(15000); 
   });
 });
